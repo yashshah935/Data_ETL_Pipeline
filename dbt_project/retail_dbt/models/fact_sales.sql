ea5683e0d000d389d3150c886d4b48d7dcde76be
yashshah935 <yashshah935@gmail.com>
2025-08-07 16:29:13 +0530

    clean code and modularise with Taskflow API in airflow

--- dbt_project/retail_dbt/models/fact_sales.sql
+++ dbt_project/retail_dbt/models/fact_sales.sql
@@ -1,6 +1,17 @@
+{{ config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='"InvoiceNo"',
+) }}
+
 SELECT
     "InvoiceNo",
     SUM("Quantity") AS total_items,
     SUM("TotalAmount") AS total_amount
 FROM {{ ref('stg_sales') }}
+{% if is_incremental() %}
+    where "InvoiceNo" not in (select "InvoiceNo" from {{ this }})
+{% else %}
+    where "InvoiceNo" is not null
+{% endif %}
 GROUP BY "InvoiceNo"
